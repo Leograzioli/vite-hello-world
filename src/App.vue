2691730a44705a34c4d7546a75b6161e150d552f
Leograzioli <leograzioli@gmail.com>
2022-11-07 17:59:05 +0100

    bonus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
+import AppJumbo from "./components/AppJumbo.vue";
+
 export default {
   data() {
     return {
       greeting: "my first vite vue page "
     }
-  }
+  },
+  components: {
+    AppJumbo
+  },
 }
 </script>
 
 <template>
-
+  <AppJumbo />
   <div> {{ greeting }}</div>
 </template>
 
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            myImage: "vue.svg"
+        }
+    },
+    methods: {
+        getPathImg() {
+            return new URL(`../assets/${this.myImage}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="getPathImg()" alt="">
+</template>
+
+<style>
+
+</style>
